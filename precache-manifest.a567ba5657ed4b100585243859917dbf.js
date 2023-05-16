0252274b71b324d5f717317f7cae10a33592b452
M1Lucken <mluckenbihl@gmail.com>
2023-05-15 20:47:07 -0700

    Updates

---
+++ precache-manifest.a567ba5657ed4b100585243859917dbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "159c9c0a8e5c9d824d6b",
+    "url": "/static/css/main.9d12e4df.chunk.css"
+  },
+  {
+    "revision": "159c9c0a8e5c9d824d6b",
+    "url": "/static/js/main.a5de8058.chunk.js"
+  },
+  {
+    "revision": "a7a93d1503069d79f58d",
+    "url": "/static/js/runtime~main.629448d9.js"
+  },
+  {
+    "revision": "c7661dc7e829f8961693",
+    "url": "/static/js/2.138eaa1e.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "61e2d62f4d7e2770f91a898f93182197",
+    "url": "/static/media/logo_spelled.61e2d62f.png"
+  },
+  {
+    "revision": "9038107b5e1c1c752561fe6e35396814",
+    "url": "/static/media/bg_gradient.9038107b.jpg"
+  },
+  {
+    "revision": "e338115ac42e437c091680cd2a8aefca",
+    "url": "/index.html"
+  }
+];
